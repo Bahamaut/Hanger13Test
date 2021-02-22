a239f6f7899973941ec831d798f447a336c1adc4
James Hanscombe <j_hanscombe@hotmail.co.uk>
2021-02-22 12:48:54 +0000

    Adding in probable View and ViewModel

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Hangar13Test.ViewModels;
+using System.Windows;
+
+namespace Hangar13Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+	{
+		private readonly MainWindowViewModel _viewModel;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			_viewModel = new MainWindowViewModel();
+			DataContext = _viewModel;
+		}
+	}
+}
